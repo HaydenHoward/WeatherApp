411d9aa89169704f15b83920a1410f01857d18c0
HaydenHoward2 <how21014@byui.edu>
2022-11-15 20:05:15 -0700

    fixed things

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+import axios from 'axios';
+
+
+function App() {
+
+  
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
+
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      });
+      setLocation('')
+    }
+  };
+
+  
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text"/>
+      </div>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            <h1>60 F</h1>
+          </div>
+          <div className="description">
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65 F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+     
+
+  );
+}
+
+export default App;
